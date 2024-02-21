b008fbb9b216348386db78a481821b38f7d8f694
BEII13 <abdushelishvili2003@gmail.com>
2024-02-21 22:25:21 +0400

    first commit

---
+++ day 2/homework 2/homework2.py
@@ -0,0 +1,7 @@
+print ("gamoviwere https://www.youtube.you/@LowBudgetStories")
+
+
+print("gavicani xalxi da davmegobrdi")
+
+
+print ("Goal Orient Academy aris ert-erti sauketeso programing centry sadac gvyavs sauketeso leqtorebi da razmis liderebi mati daxmarebit swavlis procesi aris nayofieri mxiaruli da rac mtavaria srulyofili")
