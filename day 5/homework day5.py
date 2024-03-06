a2be0271433a0d5f6ae5dbb52cdeb1bba6d53ea4
BEII13 <abdushelishvili2003@gmail.com>
2024-03-06 18:19:39 +0400

    commit

---
+++ day 5/homework day5.py
@@ -0,0 +1,34 @@
+book1 = "mikheiljavaxishvili"
+book2 = "otia ioseliani"
+book3 = "nodar dumbadze"
+book4 = "saqartvelos istoria tomi 1"
+book5 = "saqartvelos istoria tomi 2"
+book6 = "saqartvelos isotira tomi 3"
+book7 = "saqartvelos istoria tomi 4"
+book8 = "sami mushketeri"
+book9 = "shagrenis tyavi"
+book10 = "vefxis tyaosani"
+
+book1_price=9
+book2_price=12
+book3_price=15
+book4_price=20
+book5_price=20
+book6_price=20
+book7_price=20
+book8_price=5
+book9_price=6
+book10_price=50
+
+
+print (book1_price - (book1_price / 10))
+print (book2_price - (book2_price / 10))
+print (book3_price - (book3_price / 10))
+print (book4_price - (book4_price / 10))
+print (book5_price - (book5_price / 10))
+
+print (book6_price - (book6_price / 20))
+print (book7_price - (book7_price / 20))
+print (book8_price - (book8_price / 20))
+print (book9_price - (book9_price / 20))
+print (book10_price - (book10_price / 20))
